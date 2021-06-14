7704214334e8c4aeb121258ea795a83baa2f6829
Don Kackman <dkackman@gmail.com>
2021-06-14 17:37:58 -0500

    comments

--- src/chia-dotnet/Message.cs
+++ src/chia-dotnet/Message.cs
@@ -7,17 +7,48 @@ using Newtonsoft.Json.Serialization;
 namespace chia.dotnet
 {
     /// <summary>
-    /// The messaging data strucutre for request and response exchange with the RPC endpoint
+    /// The messaging data structure for request and response exchange with the RPC endpoint
     /// </summary>
     public record Message
     {
+        /// <summary>
+        /// The command to be processed by the endpoint service
+        /// </summary>
         public string Command { get; init; }
+
+        /// <summary>
+        /// Data to go along with the command
+        /// </summary>
         public dynamic Data { get; init; }
+
+        /// <summary>
+        /// The name of the origin service
+        /// </summary>
         public string Origin { get; init; }
+
+        /// <summary>
+        /// The name of the destination service
+        /// </summary>
         public string Destination { get; init; }
+
+        /// <summary>
+        /// Indication whether message is an acknowledgement (i.e response)
+        /// </summary>
         public bool Ack { get; init; }
+
+        /// <summary>
+        /// Unique id to correlate requests to responses
+        /// </summary>
         public string Request_Id { get; init; }
 
+        /// <summary>
+        /// Construct a new instance of a <see cref="Message"/>
+        /// </summary>
+        /// <param name="command"><see cref="Command"/></param>
+        /// <param name="data"><see cref="Data"/></param>
+        /// <param name="destination"><see cref="Destination"/></param>
+        /// <param name="origin"><see cref="Origin"/></param>
+        /// <returns></returns>
         public static Message Create(string command, object data, string destination, string origin)
         {
             return new Message
@@ -30,6 +61,10 @@ namespace chia.dotnet
             };
         }
 
+        /// <summary>
+        /// Serialize the <see cref="Message"/> to a json string
+        /// </summary>
+        /// <returns>Json representation of the message</returns>
         public string ToJson()
         {
             var serializerSettings = new JsonSerializerSettings
@@ -47,6 +82,11 @@ namespace chia.dotnet
             }
         }
 
+        /// <summary>
+        /// Deserialize a <see cref="Message"/> from a json string
+        /// </summary>
+        /// <param name="json">Json representation of the Message</param>
+        /// <returns><see cref="Message"/></returns>
         public static Message FromJson(string json)
         {
             var message = JsonConvert.DeserializeObject<Message>(json);
