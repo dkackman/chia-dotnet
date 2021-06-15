7ddcefe73efd45e440d2795f120edbf83e505f51
Don Kackman <dkackman@gmail.com>
2021-06-14 21:35:13 -0500

    comments

--- src/chia-dotnet/EndpointInfo.cs
+++ src/chia-dotnet/EndpointInfo.cs
@@ -7,8 +7,19 @@ namespace chia.dotnet
     /// </summary>
     public record EndpointInfo
     {
+        /// <summary>
+        /// The secure websocket (wss) Uri of the RPC endpoint
+        /// </summary>
         public Uri Uri { get; init; }
+
+        /// <summary>
+        /// The full filesystem path to the public certificate used to authenticate with the Websocket (.crt)
+        /// </summary>
         public string CertPath { get; init; }
+
+        /// <summary>
+        /// The full filesystem path to the base64 encoded RSA private key to authneticate with the websocket (.key)
+        /// </summary>
         public string KeyPath { get; init; }
     }
 }
