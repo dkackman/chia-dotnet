e2a2bf690ef54fd993b05e708661b90daa6957e0
Don Kackman <dkackman@gmail.com>
2021-07-19 20:30:40 -0500

    file and dir not found tests

--- src/chia-dotnet.tests/ConfigTests.cs
+++ src/chia-dotnet.tests/ConfigTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace chia.dotnet.tests
 {
@@ -12,6 +14,20 @@ namespace chia.dotnet.tests
             var config = Config.Open();
 
             Assert.IsNotNull(config);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void InvalidFolderThrowsDirectoryNotFound()
+        {
+            _ = Config.Open(@"C:\this\path\does\not\exist\config.yaml");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void InvalidFilenameThrowsFileNotFound()
+        {
+            _ = Config.Open(@"C:\windows\config.yaml");
         }
     }
 }
