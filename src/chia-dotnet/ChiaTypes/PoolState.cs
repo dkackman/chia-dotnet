7adabbb9ed4571e16ae0fd643ddec4c4093d869c
Don Kackman <dkackman@gmail.com>
2021-09-26 18:43:18 -0500

    make readonly

--- src/chia-dotnet/ChiaTypes/PoolState.cs
+++ src/chia-dotnet/ChiaTypes/PoolState.cs
@@ -9,19 +9,22 @@
     /// </summary>
     public record PoolState
     {
-        private string? poolUrl;
+        private readonly string? poolUrl;
 
         public byte Version { get; init; } = PoolInfo.POOL_PROTOCOL_VERSION;
+
         /// <summary>
         ///  PoolSingletonState
         /// </summary>
         public PoolSingletonState State { get; init; }
+
         /// <summary>
         /// A puzzle_hash we pay to
         /// When self-farming, this is a main wallet address
         /// When farming-to-pool, the pool sends this to the farmer during pool protocol setup
         /// </summary>
         public string TargetPuzzleHash { get; init; } = string.Empty;
+
         /// <summary>
         /// owner_pubkey is set by the wallet, once
         /// </summary>
