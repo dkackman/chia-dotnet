a15b79d15b33669e2881f7596771430e167d0703
Don Kackman <dkackman@gmail.com>
2023-01-21 19:23:26 -0600

    new properties

--- src/chia-dotnet/ChiaTypes/NFTInfo.cs
+++ src/chia-dotnet/ChiaTypes/NFTInfo.cs
@@ -17,13 +17,16 @@ namespace chia.dotnet
         public IEnumerable<string> MetadataUris { get; init; } = new List<string>();
         public string MetaataHash { get; init; } = string.Empty;
         public IEnumerable<string> LicenseUris { get; init; } = new List<string>();
-        public ulong SeriesTotal { get; init; }
-        public ulong SeriesNumber { get; init; }
+        public ulong EditionTotal { get; init; }
+        public ulong EditionNumber { get; init; }
         public string UpdaterPuzhash { get; init; } = string.Empty;
         public string ChainInfo { get; init; } = string.Empty;
         public uint MintHeight { get; init; }
         public bool SupportsDID { get; init; }
         public bool PendingTransaction { get; init; }
         public string LauncherPuzhash { get; init; } = string.Empty;
+        public string MinterDID { get; init; } = string.Empty;
+        public string OffChainMetadata { get; init; } = string.Empty;
+        public string P2Address { get; init; } = string.Empty;
     }
 }
