a9c4c49d5cc36ab52dadf0f96d6b17e96f1e0b8a
Blackcode <edragos55@yahoo.com>
2023-04-11 16:50:13 +0200

    NFTInfo fix (#76)
    * NFTInfo fix
    
    * Adding Id parameter and fix Infos for GetOfferSummary
    
    ---------
    
    Co-authored-by: Enache Dragos <edragos55@hotmail.com>

--- src/chia-dotnet/ChiaTypes/NFTInfo.cs
+++ src/chia-dotnet/ChiaTypes/NFTInfo.cs
@@ -7,26 +7,27 @@ namespace chia.dotnet
     /// </summary>
     public record NFTInfo
     {
-        public string LauncherId { get; init; } = string.Empty;
-        public string NFTCoinID { get; init; } = string.Empty;
-        public string? OwnerDID { get; init; }
-        public ushort? RoyaltyPercentage { get; init; }
-        public string? RoyaltyPuzzleHash { get; init; } = string.Empty;
-        public IEnumerable<string> DataUris { get; init; } = new List<string>();
+        public string ChainInfo { get; init; } = string.Empty;
         public string DataHash { get; init; } = string.Empty;
-        public IEnumerable<string> MetadataUris { get; init; } = new List<string>();
-        public string MetaataHash { get; init; } = string.Empty;
-        public IEnumerable<string> LicenseUris { get; init; } = new List<string>();
+        public IEnumerable<string> DataUris { get; init; } = new List<string>();
         public ulong EditionTotal { get; init; }
         public ulong EditionNumber { get; init; }
-        public string UpdaterPuzhash { get; init; } = string.Empty;
-        public string ChainInfo { get; init; } = string.Empty;
-        public uint MintHeight { get; init; }
-        public bool SupportsDID { get; init; }
-        public bool PendingTransaction { get; init; }
+        public string LauncherId { get; init; } = string.Empty;
         public string LauncherPuzhash { get; init; } = string.Empty;
-        public string MinterDID { get; init; } = string.Empty;
-        public string OffChainMetadata { get; init; } = string.Empty;
-        public string P2Address { get; init; } = string.Empty;
+        public string LicenseHash { get; init; } = string.Empty;
+        public IEnumerable<string> LicenseUris { get; init; } = new List<string>();
+        public string MetadataHash { get; init; } = string.Empty;
+        public IEnumerable<string> MetadataUris { get; init; } = new List<string>();
+        public uint MintHeight { get; init; }
+        public string? MinterDid { get; init; }
+        public string NftCoinId { get; init; } = string.Empty;
+        public object? OffChainMetadata { get; init; }
+        public string? OwnerDID { get; init; }
+        public string? P2Address { get; init; }
+        public bool PendingTransaction { get; init; }
+        public ushort? RoyaltyPercentage { get; init; }
+        public string? RoyaltyPuzzleHash { get; init; } = string.Empty;
+        public bool SupportsDID { get; init; }
+        public string UpdaterPuzhash { get; init; } = string.Empty;
     }
 }
