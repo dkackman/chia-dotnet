4efedf3941715353639363823e334e1cb24a88b0
Don Kackman <dkackman@gmail.com>
2023-07-30 14:55:08 -0500

    remove rate limited wallet types and methods

--- src/chia-dotnet/ChiaTypes/WalletInfo.cs
+++ src/chia-dotnet/ChiaTypes/WalletInfo.cs
@@ -6,7 +6,6 @@
     /// Name: can be a user provided or default generated name. (can be modified)
     /// Type: is specified during wallet creation and should never be changed.
     /// Data: this filed is intended to be used for storing any wallet specific information required for it.
-    /// (RL wallet stores origin_id, admin/user pubkey, rate limit, etc.)
     /// This data should be json encoded string.
     /// </summary>
     public record WalletInfo
