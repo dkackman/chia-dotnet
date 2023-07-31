dd143e9415f049361b9f6fde4054765cb7c47e21
kev <kev@darkhorse>
2023-07-31 11:09:37 +0100

    added initial get-block-spends

---
+++ src/chia-dotnet/ChiaTypes/BlockSpend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace chia.dotnet;
+
+public record BlockSpend
+{
+    public string Puzzle_Reveal { get; set; }
+    public string Solution { get; set; }
+    public BlockCoinSpend Coin { get; set; }
+}
+
+public record BlockCoinSpend
+{
+    public Int64 Amount { get; set; }
+    public string Parent_Coin_Info { get; set; }
+    public string Puzzle_Hash { get; set; }
+}
