20b2fcddc8eef4c4c61cf1a04c5a3e9c64ddac85
Don Kackman <dkackman@gmail.com>
2023-08-05 15:31:49 -0500

    DID stuffs

---
+++ src/chia-dotnet/ChiaTypes/DidInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace chia.dotnet
+{
+    public record DidInfo
+    {
+        public string DidId { get; init; } = string.Empty;
+        public string LastestCoin { get; init; } = string.Empty;
+        public string P2Address { get; init; } = string.Empty;
+        public string PublicKey { get; init; } = string.Empty;
+        public string RecoveryListHash { get; init; } = string.Empty;
+        public int NumVerifications { get; init; }
+        public IDictionary<string, string> Metadata { get; init; } = new Dictionary<string, string>();
+        public string LauncherId { get; init; } = string.Empty;
+        public string FullPuzzle { get; init; } = string.Empty;
+        public string Solution { get; init; } = string.Empty;
+        public IEnumerable<string> Hints { get; init; } = new List<string>();
+    }
+}
