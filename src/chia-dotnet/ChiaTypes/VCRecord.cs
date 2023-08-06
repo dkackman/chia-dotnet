28168def3c337e9141454d8c412221720bc19991
Don Kackman <dkackman@gmail.com>
2023-08-06 11:21:34 -0500

    VC stuffs

--- src/chia-dotnet/ChiaTypes/VCRecord.cs
+++ src/chia-dotnet/ChiaTypes/VCRecord.cs
@@ -2,6 +2,7 @@
 {
     public record VCRecord
     {
+        public string? CoinId { get; init; }
         public VerifiedCredential VC { get; init; } = new();
         public uint ConfirmedAtHeight { get; init; }
     }
