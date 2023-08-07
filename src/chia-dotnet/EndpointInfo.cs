9aa9594f2a93541e5aafbce7e9665a996e4582d2
Don Kackman <dkackman@gmail.com>
2023-08-07 17:20:48 -0500

    spelling

--- src/chia-dotnet/EndpointInfo.cs
+++ src/chia-dotnet/EndpointInfo.cs
@@ -13,12 +13,12 @@ namespace chia.dotnet
         public Uri Uri { get; init; } = new("http://localhost");
 
         /// <summary>
-        /// The full filesystem path to the public certificate used to authenticate with the endpoint (.crt)
+        /// The full file system path to the public certificate used to authenticate with the endpoint (.crt)
         /// </summary>
         public string CertPath { get; init; } = string.Empty;
 
         /// <summary>
-        /// The full filesystem path to the base64 encoded RSA private key to authneticate with the endpoint (.key)
+        /// The full file system path to the base64 encoded RSA private key to authenticate with the endpoint (.key)
         /// </summary>
         public string KeyPath { get; init; } = string.Empty;
     }
