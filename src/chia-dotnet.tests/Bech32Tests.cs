78275b6d65ce1d7027cabb1e882fe43e3fd7c052
kev <kev@darkhorse>
2023-08-08 21:39:38 +0100

    commented out test classes

--- src/chia-dotnet.tests/Bech32Tests.cs
+++ src/chia-dotnet.tests/Bech32Tests.cs
@@ -1,37 +1,52 @@
-﻿// using chia.dotnet.bech32;
-//
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-//
-// namespace chia.dotnet.tests
-// {
-//     [TestClass]
-//     public class Bech32Tests
-//     {
-//         [TestMethod]
-//         public void PuzzleHashToAddress()
-//         {
-//             var bech32m = new Bech32M("xch");
-//             var address = bech32m.PuzzleHashToAddress("0xdb96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66");
-//
-//             Assert.AreEqual("xch1mwt0ym6jgkawc0zwqmdzrkw4pjr337vjpd0fx4xr4ym0crhynfnq96pztp", address);
-//         }
-//
-//         [TestMethod]
-//         public void AddressToPuzzleHash()
-//         {
-//             var hash = Bech32M.AddressToPuzzleHashString("xch1mwt0ym6jgkawc0zwqmdzrkw4pjr337vjpd0fx4xr4ym0crhynfnq96pztp");
-//
-//             Assert.AreEqual("db96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66", hash);
-//         }
-//
-//         [TestMethod]
-//         public void ZeroXPrefixIsIgnored()
-//         {
-//             var bech32m = new Bech32M("xch");
-//             var addressWithPrefix = bech32m.PuzzleHashToAddress("0xdb96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66");
-//             var addressWithoutPrefix = bech32m.PuzzleHashToAddress("db96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66");
-//
-//             Assert.AreEqual(addressWithPrefix, addressWithoutPrefix);
-//         }
-//     }
-// }
+﻿using chia.dotnet.bech32;
+using chia.dotnet.tests.Core;
+using Xunit;
+
+namespace chia.dotnet.tests
+{
+    public class Bech32Tests : TestBase
+    {
+        public Bech32Tests(ChiaDotNetFixture fixture) : base(fixture)
+        {
+        }
+        
+        [Fact]
+        public void PuzzleHashToAddress()
+        {
+            // Arrange
+            var bech32m = new Bech32M("xch");
+            
+            // Act
+            var address = bech32m.PuzzleHashToAddress("0xdb96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66");
+
+            // Assert
+            Assert.Equal("xch1mwt0ym6jgkawc0zwqmdzrkw4pjr337vjpd0fx4xr4ym0crhynfnq96pztp", address);
+        }
+
+        [Fact]
+        public void AddressToPuzzleHash()
+        {
+            // Arrange
+            
+            // Act
+            var hash = Bech32M.AddressToPuzzleHashString("xch1mwt0ym6jgkawc0zwqmdzrkw4pjr337vjpd0fx4xr4ym0crhynfnq96pztp");
+
+            // Assert
+            Assert.Equal("db96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66", hash);
+        }
+
+        [Fact]
+        public void ZeroXPrefixIsIgnored()
+        {
+            // Arrange
+            var bech32m = new Bech32M("xch");
+            
+            // Act
+            var addressWithPrefix = bech32m.PuzzleHashToAddress("0xdb96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66");
+            var addressWithoutPrefix = bech32m.PuzzleHashToAddress("db96f26f5245baec3c4e06da21d9d50c8718f9920b5e9354c3a936fc0ee49a66");
+
+            // Assert
+            Assert.Equal(addressWithPrefix, addressWithoutPrefix);
+        }
+    }
+}
