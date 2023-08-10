c5e2f763991b684d361a3682b42c6c2add4223ba
Don Kackman <dkackman@gmail.com>
2023-08-10 16:20:02 -0500

    test stubber (#110)

---
+++ src/CodeGen/TestStub.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace CodeGen
+{
+    internal class TestStub
+    {
+        public static void GenerateTestStub(string assemblyFilePath, string classUnderTestName)
+        { 
+            var assembly = Assembly.LoadFile(assemblyFilePath);
+            var classUnderTest = assembly.GetType(classUnderTestName);
+
+        }
+    }
+}
