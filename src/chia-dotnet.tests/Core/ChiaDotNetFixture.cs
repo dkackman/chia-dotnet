66279a8756fb75e010e8e3bfac661370a50ed820
kev <kev@darkhorse>
2023-08-11 15:44:01 +0100

    migrate daemon tests

--- src/chia-dotnet.tests/Core/ChiaDotNetFixture.cs
+++ src/chia-dotnet.tests/Core/ChiaDotNetFixture.cs
@@ -105,6 +105,7 @@ public class ChiaDotNetFixture : IDisposable
                         var walletHttpClient = new HttpRpcClient(walletEndpointInfo);
                         var harvesterHttpClient = new HttpRpcClient(harvesterEndpointInfo);
                         var crawlerHttpClient = new HttpRpcClient(crawlerEndpointInfo);
+                        var daemonHttpClient = new HttpRpcClient(daemonEndpointInfo);
 
                         //Proxies
                         var nodeRpcClient = new FullNodeProxy(nodeHttpClient, OriginService);
