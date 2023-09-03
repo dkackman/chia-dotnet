be9d388ba61e9842ce3130c20a37205916876e12
Don Kackman <dkackman@gmail.com>
2023-09-03 08:53:26 -0500

    use HexBtes for coin name calculation

--- src/chia-dotnet/ChiaTypes/Coin.cs
+++ src/chia-dotnet/ChiaTypes/Coin.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Linq;
-using System.Security.Cryptography;
+using chia.dotnet.bech32;
 
 namespace chia.dotnet
 {
@@ -13,26 +11,24 @@ namespace chia.dotnet
         public string ParentCoinInfo { get; init; } = string.Empty;
         public string PuzzleHash { get; init; } = string.Empty;
         public ulong Amount { get; init; }
+
+        [JsonIgnore]
+        public string AmountHex
+        {
+            get
+            {
+                var amountHexString = Amount.ToString("X");
+                return (amountHexString.Length % 2 == 0 ? "" : "0") + amountHexString;
+            }
+        }
+
         [JsonIgnore]
         public string Name
         {
             get
             {
-                // convert values to bytes
-                var parent_id_bytes = Convert.FromHexString(ParentCoinInfo);
-                var puzzle_hash_bytes = Convert.FromHexString(PuzzleHash);
-                var amount_hex_string = Amount.ToString("X");
-                // add leading zero if needed
-                amount_hex_string = (amount_hex_string.Length % 2 == 0 ? "" : "0") + amount_hex_string;
-                var amount_bytes = Convert.FromHexString(amount_hex_string);
-
-                // concat all to one bytes array
-                var bytes = parent_id_bytes.Concat(puzzle_hash_bytes).Concat(amount_bytes).ToArray();
-
-                using var hash = SHA256.Create();
-                var coin_id_bytes = hash.ComputeHash(bytes);
-
-                return Convert.ToHexString(coin_id_bytes);
+                var theBytes = HexBytes.FromHex(ParentCoinInfo) + HexBytes.FromHex(PuzzleHash) + HexBytes.FromHex(AmountHex);
+                return theBytes.Sha256().ToString().ToUpperInvariant();
             }
         }
     }
